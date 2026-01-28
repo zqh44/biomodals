2b94dc70cb58ea6c8411c96b1d8fecf0d2799cbd
zqh44 <58357309+zqh44@users.noreply.github.com>
2026-01-28 23:32:02 +0800

    Create hello.py
    test

---
+++ biomodals/app/design/hello.py
@@ -0,0 +1,32 @@
+import sys
+
+import modal
+
+app = modal.App("example-hello-world")
+
+@app.function()
+def f(i):
+    if i % 2 == 0:
+        print("hello", i)
+    else:
+        print("world", i, file=sys.stderr)
+
+    return i * i
+
+@app.local_entrypoint()
+def main():
+    # run the function locally
+    print(f.local(1000))
+
+    # run the function remotely on Modal
+    print(f.remote(1000))
+
+    # run the function in parallel and remotely on Modal
+    total = 0
+    for ret in f.map(range(200)):
+        total += ret
+
+    print(total)
+
+
+    
